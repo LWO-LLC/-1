41224881750677bc3e3cee7cd7cfd4bfce54d725
Demonchannel <69786423+Demonchannel@users.noreply.github.com>
2020-08-24 13:15:53 +0300

    Add files via upload

---
+++ ДЗ3/Вариант 2/Objects.js
@@ -0,0 +1,64 @@
+let user = new Object;
+let user = {
+  name: "John";
+  age: 30;
+  user.isAdmin = true;
+  user["likes birds"] = true;
+};
+let animal = new Object;
+let animal = {
+  name: "Strike";
+  age: 12;
+  user.isAdmin = false;
+  animal["likes birds"] = true;
+};
+let cat = new Object;
+let cat = {
+  name: "Sarra";
+  age: 8;
+  user.isAdmin = true;
+  cat["likes birds"] = true;
+};
+let dog = new Object;
+let dog = {
+  name: "John";
+  age: 30;
+  user.isAdmin = true;
+  user["likes birds"] = true;
+};
+let user5 = new Object;
+let user5 = {
+  name: "Taddy";
+  age: 34;
+  user.isAdmin = true;
+  user["likes birds"] = true;
+};
+let user4 = new Object;
+let user4 = {
+  name: "Sonic";
+  age: 28;
+  user.isAdmin = false;
+  user["likes birds"] = true;
+};
+let user3 = new Object;
+let user3 = {
+  name: "Ricardo";
+  age: 34;
+  user.isAdmin = false;
+  user["likes birds"] = true;
+};
+let user2 = new Object;
+let user2 = {
+  name: "Sara";
+  age: 32;
+  user.isAdmin = true;
+  user["likes birds"] = false;
+};
+let myCar = new Object();
+myCar.make = "Ford";
+myCar.model = "Mustang";
+myCar.year = 1969;
+let myHouse = new Object();
+myHouse.make = "Son";
+myHouse.model = "Bulba";
+myHouse.year = 1969;
